5d9f50e67e2478bbb79d200aaf20e5810e4581d4
vivek <vivek@GDC-VivekM-LPT.agreeya.local>
2018-12-28 17:10:05 +0530

    TTA_Home

---
+++ Scripts/TTA_Home/Script1545997186599.groovy
@@ -0,0 +1,141 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://talktoastro.com/')
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_home'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_astrologers'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Customer stories'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_articles'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Consult Now'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_home'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/h4_Dr Ajay Kumar Sharma'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_home'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/h4_Upma Shrivastava'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_home'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/h4_Reema Comar'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_home'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/h4_Dr. Umesh Dwivedii'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_home'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_1'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_2'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_3'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_4'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_5'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_6'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_7'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_8'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_9'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_10'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_11'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Monthly'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Aries'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Taurus'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Gemini'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Cancer'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/p_Leo'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/p_Leo'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_12'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_13'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_14'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_15'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_16'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_17'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_18'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_19'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Yearly'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Taurus_1'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Gemini_1'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Cancer_1'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Leo'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Virgo'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Libra'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/li_Scorpio'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Scorpio'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Sagittarius'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Capricorn'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Aquarius'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Pisces'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_About us'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Terms  Conditions'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Privacy Policy'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/a_Contact us'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_20'))
+
+WebUI.click(findTestObject('Object Repository/Page_Talk to Astro - Online Astrolo/img_21'))
+
+WebUI.closeBrowser()
+
